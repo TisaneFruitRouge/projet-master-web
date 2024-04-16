609e9ab3936b7a5dcce70051fe200ca4bd4f7a9f
benzahia <benzahiamo@gmail.com>
2024-04-16 10:50:49 +0200

    first table render

--- front/app/statistics/global/page.tsx
+++ front/app/statistics/global/page.tsx
@@ -1,7 +1,26 @@
-export default function GlobalStats() {
+
+import { Payment, columns } from "./columns"
+import { DataTable } from "./data-table"
+
+async function getData(): Promise<Payment[]> {
+    // Fetch data from your API here.
+    return [
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "m@example.com",
+        },
+        // ...
+    ]
+}
+
+export default async function GlobalStats() {
+    const data = await getData()
+
     return (
-        <div>
-            Global Stats page
+        <div className="container mx-auto py-10">
+            <DataTable columns={columns} data={data} />
         </div>
     )
 }
